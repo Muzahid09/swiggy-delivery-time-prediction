92a33769bed7ea1958f378bd68f1649d429579d0
muzahid <muzahid023@icloud.com>
2024-12-27 12:28:28 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Biuld a ML service that predicts food delivery time',
+    author='Muzahid',
+    license='',
+)

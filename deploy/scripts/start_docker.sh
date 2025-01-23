686c57f25cc43b614375ba177eb4b0bdbfbd1bf4
Muzahid09 <muzahid023@icloud.com>
2025-01-24 03:42:11 +0530

    added some changes in appspec file and deploy scripts

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+exec > /home/ubuntu/start_docker.log 2>&1
 
 # login in ecr
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 730335644260.dkr.ecr.ap-south-1.amazonaws.com
@@ -10,14 +11,14 @@ docker pull 730335644260.dkr.ecr.ap-south-1.amazonaws.com/food_repo:v1
 
 
 # Check if the container is already running
-if [[ $(sudo docker ps -aq -f name=ec2_first_container) ]]
-then
-  # Stop and remove the existing container
-  sudo docker stop ec2_first_container
-  sudo docker rm ec2_first_container
+if [ "$(docker ps -q -f name=ec2_first_container)" ]; then
+    docker stop ec2_first_container
+fi
+
+if [ "$(docker ps -aq -f name=ec2_first_container)" ]; then
+    docker rm ec2_first_container
 fi
 
 
-
 # Run the Docker container
-sudo docker run -d -p 8000:8000 -e DAGSHUB_USER_TOKEN=705171615cb002f197303ee05d8813d5f80e089a --name ec2_first_container 730335644260.dkr.ecr.ap-south-1.amazonaws.com/food_repo:v1
+sudo docker run -d -p 80:8000 -e DAGSHUB_USER_TOKEN=705171615cb002f197303ee05d8813d5f80e089a --name ec2_first_container 730335644260.dkr.ecr.ap-south-1.amazonaws.com/food_repo:v1
